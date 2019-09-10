567aee8b2e942de9668b00cde52a7c467d2a5f2a
Florent Kermarrec <florent@enjoy-digital.fr>
2019-09-10 12:02:41 +0200

    usddrphy_mig: start connecting things

--- usddrphy_mig.py
+++ usddrphy_mig.py
@@ -1,137 +1,122 @@
 # This file is Copyright (c) 2019 Florent Kermarrec <florent@enjoy-digital.fr>
 # License: BSD
 
-import math
-from collections import OrderedDict
-
 from migen import *
-from migen.genlib.misc import BitSlip, WaitTimer
-
-from litex.soc.interconnect.csr import *
-
-from litedram.common import PhySettings
-from litedram.phy.dfi import *
 
 
 class USDDRPHY(Module, AutoCSR):
-    def __init__(self, pads, memtype="DDR4", sys_clk_freq=100e6, iodelay_clk_freq=200e6, cmd_latency=0):
-        tck = 2/(2*4*sys_clk_freq)
-        addressbits = len(pads.a)
-        if memtype == "DDR4":
-            addressbits += 3 # cas_n/ras_n/we_n multiplexed with address
-        bankbits = len(pads.ba) if memtype == "DDR3" else len(pads.ba) + len(pads.bg)
-        nranks = 1 if not hasattr(pads, "cs_n") else len(pads.cs_n)
-        databits = len(pads.dq)
-        nphases = 4
-
-        # compute phy settings
-        cl, cwl = get_cl_cw(memtype, tck)
-        cwl = cwl + cmd_latency
-        cl_sys_latency = get_sys_latency(nphases, cl)
-        cwl_sys_latency = get_sys_latency(nphases, cwl)
-
-        rdcmdphase, rdphase = get_sys_phases(nphases, cl_sys_latency, cl)
-        wrcmdphase, wrphase = get_sys_phases(nphases, cwl_sys_latency, cwl)
-        self.settings = PhySettings(
-            memtype=memtype,
-            databits=databits,
-            dfi_databits=2*databits,
-            nranks=nranks,
-            nphases=nphases,
-            rdphase=rdphase,
-            wrphase=wrphase,
-            rdcmdphase=rdcmdphase,
-            wrcmdphase=wrcmdphase,
-            cl=cl,
-            cwl=cwl - cmd_latency,
-            read_latency=2 + cl_sys_latency + 1 + 3,
-            write_latency=cwl_sys_latency
-        )
-
-#        self.dfi = Interface(addressbits, bankbits, nranks, 2*databits, nphases)
-#        if memtype == "DDR3":
-#            _dfi = self.dfi
-#        else:
-#            _dfi = Interface(addressbits, bankbits, nranks, 2*databits, nphases)
-#            dfi_mux = DDR4DFIMux(self.dfi, _dfi)
-#            self.submodules += dfi_mux
+    def __init__(self, platform):
 
         # # #
 
+        sys_rst = platform.request("cpu_reset")
+        sys_clk = platform.request("clk300")
+
+        self.dbg_clk = Signal()
+        self.dbg_rd_data_cmp = Signal(64)
+        self.dbg_expected_data = Signal(64)
+        self.dbg_cal_seq = Signal(3)
+        self.dbg_cal_seq_cnt = Signal(32)
+        self.dbg_cal_seq_rd_cnt = Signal(8)
+        self.dbg_rd_valid = Signal()
+        self.dbg_cmp_byte = Signal(6)
+        self.dbg_rd_data = Signal(64)
+        self.dbg_cplx_config = Signal(16)
+        self.dbg_cplx_status = Signal(2)
+        self.dbg_io_address = Signal(28)
+        self.dbg_pllGate = Signal()
+        self.dbg_phy2clb_fixdly_rdy_low = Signal(20)
+        self.dbg_phy2clb_fixdly_rdy_upp = Signal(20)
+        self.dbg_phy2clb_phy_rdy_low = Signal(20)
+        self.dbg_phy2clb_phy_rdy_upp = Signal(20)
+        self.win_status = Signal(32)
+        self.cal_r0_status = Signal(128)
+        self.cal_post_status = Signal(9)
+        self.tCWL = Signal(6)
+
         self.specials += Instance("ddr4_0",
-            i_sys_rst=,
-            i_c0_sys_clk_p=,
-            i_c0_sys_clk_n=,
-            o_c0_ddr4_ui_clk=,
-            o_c0_ddr4_ui_clk_sync_rst=,
+            # Clk/Rst ------------------------------------------------------------------------------
+            i_sys_rst=sys_rst,
+            i_c0_sys_clk_p=sys_clk,
+            i_c0_sys_clk_n=sys_clk,
+            o_c0_ddr4_ui_clk=ClockSignal("sys"),
+            o_c0_ddr4_ui_clk_sync_rst=ResetSignal("sys"),
+
+            # DRAM pads ----------------------------------------------------------------------------
+            o_c0_ddr4_act_n=pads.act_n,
+            o_c0_ddr4_adr=pads.a,
+            o_c0_ddr4_ba=pads.ba,
+            o_c0_ddr4_bg=pads.bg,
+            o_c0_ddr4_cke=pads.cke,
+            o_c0_ddr4_odt=pads.odt,
+            o_c0_ddr4_cs_n=pads.cs_n,
+            o_c0_ddr4_ck_t=pads.clk_p,
+            o_c0_ddr4_ck_c=pads.clk_n,
+            o_c0_ddr4_reset_n=pads.reset_n,
+            io_c0_ddr4_dm_dbi_n=pads.dm,
+            io_c0_ddr4_dq=pads.dq,
+            io_c0_ddr4_dqs_c=pads.dqs_p,
+            io_c0_ddr4_dqs_t=pads.dqs_n,
+
+
+            # Calibration --------------------------------------------------------------------------
             o_c0_init_calib_complete=,
 
-            o_dbg_clk=,
-            o_dbg_rd_data_cmp=,
-            o_dbg_expected_data=,
-            o_dbg_cal_seq=,
-            o_dbg_cal_seq_cnt=,
-            o_dbg_cal_seq_rd_cnt=,
-            o_dbg_rd_valid=,
-            o_dbg_cmp_byte=,
-            o_dbg_rd_data=,
-            o_dbg_cplx_config=,
-            o_dbg_cplx_status=,
-            o_dbg_io_address=,
-            o_dbg_pllGate=,
-            o_dbg_phy2clb_fixdly_rdy_low=,
-            o_dbg_phy2clb_fixdly_rdy_upp=,
-            o_dbg_phy2clb_phy_rdy_low=,
-            o_dbg_phy2clb_phy_rdy_upp=,
-            o_win_status=,
-            o_cal_r0_status=,
-            o_cal_post_status=,
+            # Debug --------------------------------------------------------------------------------
+            o_dbg_clk=self.dbg_clk,
+            o_dbg_rd_data_cmp=self.dbg_rd_data_cmp,
+            o_dbg_expected_data=self.dbg_expected_data,
+            o_dbg_cal_seq=self.dbg_cal_seq,
+            o_dbg_cal_seq_cnt=self.dbg_cal_seq_cnt,
+            o_dbg_cal_seq_rd_cnt=self.dbg_cal_seq_rd_cnt,
+            o_dbg_rd_valid=self.dbg_rd_valid,
+            o_dbg_cmp_byte=self.dbg_cmp_byte,
+            o_dbg_rd_data=self.dbg_rd_data,
+            o_dbg_cplx_config=self.dbg_cplx_config,
+            o_dbg_cplx_status=self.dbg_cplx_status,
+            o_dbg_io_address=self.dbg_io_address,
+            o_dbg_pllGate=self.dbg_pllGate,
+            o_dbg_phy2clb_fixdly_rdy_low=self.dbg_phy2clb_fixdly_rdy_low,
+            o_dbg_phy2clb_fixdly_rdy_upp=self.dbg_phy2clb_fixdly_rdy_upp,
+            o_dbg_phy2clb_phy_rdy_low=self.dbg_phy2clb_phy_rdy_low,
+            o_dbg_phy2clb_phy_rdy_upp=self.dbg_phy2clb_phy_rdy_upp,
+            o_win_status=self.win_status,
+            o_cal_r0_status=self.cal_r0_status,
+            o_cal_post_status=self.cal_post_status,
+            #o_dbg_bus=,
+            o_tCWL=self.tCWL,
 
+            # ??? ----------------------------------------------------------------------------------
             o_addn_ui_clkout1=,
 
-            o_c0_ddr4_act_n=,
-            o_c0_ddr4_adr=,
-            o_c0_ddr4_ba=,
-            o_c0_ddr4_bg=,
-            o_c0_ddr4_cke=,
-            o_c0_ddr4_odt=,
-            o_c0_ddr4_cs_n=,
-            o_c0_ddr4_ck_t=,
-            o_c0_ddr4_ck_c=,
-            o_c0_ddr4_reset_n=,
-            io_c0_ddr4_dm_dbi_n=,
-            io_c0_ddr4_dq=,
-            io_c0_ddr4_dqs_c=,
-            io_c0_ddr4_dqs_t=,
+            # Memory Controller Interface ----------------------------------------------------------
+            i_dBufAdr=0, # Reserved, should be tied to 0
+            i_wrData=0,
+            i_wrDataMask=0,
+            #o_rdData=,
+            #o_rdDataAddr=,
+            #o_rdDataEn=,
+            #o_rdDataEnd=,
+            #o_per_rd_done=,
+            #o_rmw_rd_done=,
+            #o_wrDataAddr=,
+            #o_wrDataEn=,
 
-            i_dBufAdr=,
-            i_wrData=,
-            i_wrDataMask=,
-            o_rdData=,
-            o_rdDataAddr=,
-            o_rdDataEn=,
-            o_rdDataEnd=,
-            o_per_rd_done=,
-            o_rmw_rd_done=,
-            o_wrDataAddr=,
-            o_wrDataEn=,
-
-            i_mc_ACT_n=,
-            i_mc_ADR=,
-            i_mc_BA=,
-            i_mc_BG=,
-            i_mc_CKE=,
-            i_mc_CS_n=,
-            i_mc_ODT=,
-            i_mcCasSlot=,
-            i_mcCasSlot2=,
-            i_mcRdCAS=,
-            i_mcWrCAS=,
-            i_winInjTxn=,
-            i_winRmw=,
-            i_gt_data_ready=,
-            i_winBuf=,
-            i_winRank=,
-            o_tCWL=,
-            o_dbg_bus=,
+            i_mc_ACT_n=0b11111111,
+            i_mc_ADR=0,
+            i_mc_BA=0,
+            i_mc_BG=0,
+            i_mc_CKE=0,
+            i_mc_CS_n=0b11111111,
+            i_mc_ODT=0b11111111,
+            i_mcCasSlot=0,
+            i_mcCasSlot20=,
+            i_mcRdCAS=0,
+            i_mcWrCAS=0,
+            i_winInjTxn=0,
+            i_winRmw=0,
+            i_gt_data_ready=0,
+            i_winBuf=0,
+            i_winRank=0,
         )
+        platform.add_ip(os.path.join("ip", "ddr4_0.xci"))
