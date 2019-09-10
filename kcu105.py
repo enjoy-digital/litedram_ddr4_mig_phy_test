a9ea8bce9b2181bea5705f32d39755f2f6aacdf0
Florent Kermarrec <florent@enjoy-digital.fr>
2019-09-10 15:09:14 +0200

    wip

--- kcu105.py
+++ kcu105.py
@@ -40,28 +40,28 @@ class DDR4TestSoC(SoCMini):
         analyzer_signals = [
             ddr4_phy.c0_ddr4_ui_clk,
             ddr4_phy.c0_ddr4_ui_clk_sync_rst,
-            ddr4_phy.c0_init_calib_complete,
+            ddr4_phy.c0_init_calib_complete,        # Signifies the status of calibration
 
             ddr4_phy.dbg_clk,
-            ddr4_phy.dbg_rd_data_cmp,
-            ddr4_phy.dbg_expected_data,
-            ddr4_phy.dbg_cal_seq,
-            ddr4_phy.dbg_cal_seq_cnt,
-            ddr4_phy.dbg_cal_seq_rd_cnt,
-            ddr4_phy.dbg_rd_valid,
-            ddr4_phy.dbg_cmp_byte,
-            ddr4_phy.dbg_rd_data,
-            ddr4_phy.dbg_cplx_config,
-            ddr4_phy.dbg_cplx_status,
-            ddr4_phy.dbg_io_address,
-            ddr4_phy.dbg_pllGate,
-            ddr4_phy.dbg_phy2clb_fixdly_rdy_low,
-            ddr4_phy.dbg_phy2clb_fixdly_rdy_upp,
-            ddr4_phy.dbg_phy2clb_phy_rdy_low,
-            ddr4_phy.dbg_phy2clb_phy_rdy_upp,
-            ddr4_phy.win_status,
-            ddr4_phy.cal_r0_status,
-            ddr4_phy.cal_post_status,
+            ddr4_phy.dbg_rd_data_cmp,                # Comparison of dbg_rd_data and dbg_expected_data
+            ddr4_phy.dbg_expected_data,              # Displays the expected data during calibration stages that use fabric-based data pattern comparison such as Read per
+            ddr4_phy.dbg_cal_seq,                    # Calibration sequence indicator, when RTL is issuing commands to the DRAM.
+            ddr4_phy.dbg_cal_seq_cnt,                # Calibration command sequence count used when RTL is issuing commands to the DRAM.
+            ddr4_phy.dbg_cal_seq_rd_cnt,             # Calibration read data burst count
+            ddr4_phy.dbg_rd_valid,                   # Read data valid
+            ddr4_phy.dbg_cmp_byte,                   # Calibration byte selection
+            ddr4_phy.dbg_rd_data,                    # Read data from input FIFOs
+            ddr4_phy.dbg_cplx_config,                # Complex cal configuration
+            ddr4_phy.dbg_cplx_status,                # Complex cal status
+            ddr4_phy.dbg_io_address,                 # MicroBlaze I/O address bus
+            ddr4_phy.dbg_pllGate,                    # PLL lock indicator
+            ddr4_phy.dbg_phy2clb_fixdly_rdy_low,     # Xiphy fixed delay ready signal (lower nibble)
+            ddr4_phy.dbg_phy2clb_fixdly_rdy_upp,     # Xiphy fixed delay ready signal (upper nibble)
+            ddr4_phy.dbg_phy2clb_phy_rdy_low,        # Xiphy phy ready signal (lower nibble)
+            ddr4_phy.dbg_phy2clb_phy_rdy_upp,        # Xiphy phy ready signal (upper nibble)
+            ddr4_phy.win_status,                     #
+            ddr4_phy.cal_r0_status,                  # Signifies the status of each stage of calibration.
+            ddr4_phy.cal_post_status,                # Signifies the status of calibration.
             ddr4_phy.tCWL,
         ]
         self.submodules.analyzer = LiteScopeAnalyzer(analyzer_signals, 128, csr_csv="analyzer.csv")
